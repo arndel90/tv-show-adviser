2142f9e1bc4f3365cea6cf1145074b6d247556dc
Arnaud Delplanque <arndel90@hotmail.com>
2024-07-30 12:29:34 +0200

    add env file for api key

--- src/api/tv-shows.js
+++ src/api/tv-shows.js
@@ -1,19 +1,20 @@
 ﻿import axios from 'axios'
-import {API_KEY_PARAM, BASE_URL} from "../config.js";
+import {BASE_URL} from "../config.js";
 
 export default class TvShowsAPI {
+    static API_KEY = import.meta.env.VITE_API_KEY
     static async fetchPopulars() {
-        const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM}`)
+        const response = await axios.get(`${BASE_URL}tv/popular?api_key=${import.meta.env.VITE_API_KEY}`)
         return response.data.results
     }
     
     static async fetchRecommendations(tvShowId) {
-        const response = await axios.get(`${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM}`)
+        const response = await axios.get(`${BASE_URL}tv/${tvShowId}/recommendations?api_key=${import.meta.env.VITE_API_KEY}`)
         return response.data.results
     }
 
     static async fetchByTitle(title) {
-        const response = await axios.get(`${BASE_URL}search/tv${API_KEY_PARAM}&query=${title}`)
+        const response = await axios.get(`${BASE_URL}search/tv?api_key=${import.meta.env.VITE_API_KEY}&query=${title}`)
         return response.data.results
     }
 }
